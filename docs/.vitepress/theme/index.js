b776e0c8af0dbde3624e097d1c53a539508a6334
chenxianyin <iamhongren@gmail.com>
2026-03-04 09:54:45 +0800

    no message

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,16 @@
 // .vitepress/theme/index.js
-
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import './custom.css'
 import Giscus from './giscus.vue'
+
 export default {
-    extends: DefaultTheme,
-    Layout: () => h(Giscus)
+    ...DefaultTheme,
+    // 布局增强：在文档底部添加评论组件
+    Layout() {
+        return h(DefaultTheme.Layout, null, {
+            // 插槽：doc-after 表示在文档内容之后渲染
+            'doc-after': () => h(Giscus)
+        })
+    }
 }
